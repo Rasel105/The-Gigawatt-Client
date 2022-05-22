43048d32daa8408d1b4fa6e97d246b4a93970fbf
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-22 12:12:01 +0600

    banner and navbar created

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row mt-20">
+                <div>
+                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+                <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-md rounded-lg shadow-2xl" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const NavItem = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Review</Link></li>
+        <li><Link to="/">Dashboard</Link></li>
+        <li><Link to="/">Contact</Link></li>
+        <li><Link to="/">Login</Link></li>
+    </>
+    return (
+        <div class="navbar bg-base-100 fixed z-30 px-12">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {NavItem}
+                    </ul>
+                </div>
+                <Link to="/" class="btn btn-ghost normal-case text-xl">The Gigawatt</Link>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                    {NavItem}
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <a class="btn">Get started</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;

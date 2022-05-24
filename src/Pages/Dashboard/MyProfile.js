bee238a68b0f58f9c6d3615d1dcb54f03aedfcf1
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-24 07:22:44 +0600

    react query added

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MyProfile = () => {
     return (
         <div>
-            <h2 className='pt-40'>My profile</h2>
+            <h2 className='pt-25 text-2xl text-center'>My profile</h2>
         </div>
     );
 };

26674dd7a4f3d022899d99f0260d6d5d70c7d755
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-26 15:45:22 +0600

    review section done

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,35 +1,18 @@
-import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
 import ReviewCard from '../Home/ReviewCard'
 const Reviews = () => {
     const [reviews, setReviews] = useState([])
-    const [user] = useAuthState(auth);
-    const navigate = useNavigate();
+
     useEffect(() => {
-        if (user) {
-            fetch(`http://localhost:5000/reviews?email=${user.email}`, {
-                method: "GET",
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem("accessToken")}`
-                }
+        fetch(`http://localhost:5000/reviews`)
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+                console.log(data)
             })
-                .then(res => {
-                    if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem("accessToken");
-                        navigate('/');
-                    }
-                    return res.json()
-                })
-                .then(data => {
-                    setReviews(data)
-                    console.log(data)
-                })
-        }
-    }, [user, navigate])
+    }, [])
+
+
     return (
         <>
             <h2 className='text-3xl text-center pt-20'><span className='text-primary'>Review</span> {reviews.length}</h2>

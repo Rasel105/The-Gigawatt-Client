d53f6c2572ec99e97e9bfa2a2ddd3c3063681807
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-26 11:28:27 +0600

    paid order show and button hide

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MyOrder = ({ order, setDeletingItem, index }) => {
-    const { _id, userName, email, item, minimumOrder, totalPrice } = order;
+    const { _id, userName, paid, email, item, minimumOrder, totalPrice } = order;
     console.log(order);
     return (
         <>
@@ -13,8 +13,11 @@ const MyOrder = ({ order, setDeletingItem, index }) => {
                 <td>{item}</td>
                 <td>{totalPrice}</td>
                 <td>{minimumOrder}/pcs</td>
-                <td><Link to={`/dashboard/payment/${_id}`} className='btn btn-success text-white'>Pay</Link></td>
-                <td><label onClick={() => setDeletingItem(order)} htmlFor="all-info" className="btn btn-error text-white modal-button">Delete</label></td>
+                <td>
+                    {/* <Link to={`/dashboard/payment/${_id}`} className='btn btn-success text-white'>Pay</Link> */}
+                    {(totalPrice && !paid) && <Link to={`/dashboard/payment/${_id}`}><button className='btn btn-xs btn-success text-white'>Pay</button></Link>}
+                </td>
+                {!paid && <td><label onClick={() => setDeletingItem(order)} htmlFor="all-info" className="btn btn-error btn-xs text-white modal-button">Delete</label></td>}
             </tr>
         </>
     );

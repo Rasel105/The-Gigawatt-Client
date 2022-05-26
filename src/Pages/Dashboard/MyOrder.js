0a7beab7d2d02a8bc949665a6f4de9357f8c86a7
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-26 19:01:46 +0600

    fool site animated

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Zoom from 'react-reveal/Zoom';
 
 const MyOrder = ({ order, setDeletingItem, index }) => {
     const { _id, userName, paid, email, item, minimumOrder, totalPrice, transactionId } = order;
@@ -7,20 +8,22 @@ const MyOrder = ({ order, setDeletingItem, index }) => {
     return (
         <>
             <tr>
-                <th>{index + 1}</th>
-                <td>{userName}</td>
-                <td>{email}</td>
-                <td>{item}</td>
-                <td>${totalPrice}</td>
-                <td>{minimumOrder}/pcs</td>
-                <td>
-                    {(!paid) && <Link to={`/dashboard/payment/${_id}`}><button className='btn btn-xs btn-success text-white'>Pay</button></Link>}
-                    {(paid) && <div>
-                        <p><button className='btn btn-xs btn-primary text-white'>Paid</button></p>
-                        <p>Transaction Id: <span className='text-success'>{transactionId}</span> </p>
-                    </div>}
-                </td>
-                {!paid && <td><label onClick={() => setDeletingItem(order)} htmlFor="all-info" className="btn btn-error btn-xs text-white modal-button">Delete</label></td>}
+                <Zoom left cascade>
+                    <th>{index + 1}</th>
+                    <td>{userName}</td>
+                    <td>{email}</td>
+                    <td>{item}</td>
+                    <td>${totalPrice}</td>
+                    <td>{minimumOrder}/pcs</td>
+                    <td>
+                        {(!paid) && <Link to={`/dashboard/payment/${_id}`}><button className='btn btn-xs btn-success text-white'>Pay</button></Link>}
+                        {(paid) && <div>
+                            <p><button className='btn btn-xs btn-primary text-white'>Paid</button></p>
+                            <p>Transaction Id: <span className='text-success'>{transactionId}</span> </p>
+                        </div>}
+                    </td>
+                    {!paid && <td><label onClick={() => setDeletingItem(order)} htmlFor="all-info" className="btn btn-error btn-xs text-white modal-button">Delete</label></td>}
+                </Zoom>
             </tr>
         </>
     );

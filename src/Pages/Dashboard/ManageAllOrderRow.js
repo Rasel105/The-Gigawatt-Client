df861fcca649abc407ce66105b99c0c93ab40457
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-26 22:53:45 +0600

    btton changes

--- src/Pages/Dashboard/ManageAllOrderRow.js
+++ src/Pages/Dashboard/ManageAllOrderRow.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
+import { toast } from 'react-toastify';
 
 const ManageAllOrderRow = ({ order, index, refetch, setDeletingItem }) => {
-    const { address, paid, item, minimumOrder, phone, totalPrice, userName, email } = order;
+    const { _id, address, paid, status, item, minimumOrder, phone, totalPrice, userName, email } = order;
+    const handleChangeStatus = (id) => {
+        const status = "shipped";
+        fetch(`http://localhost:5000/shipped/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({ status }),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success("Product Shipped");
+                console.log(data);
+                console.log(order);
+                refetch();
+            });
+    }
     return (
         <tr class="hover">
             <Zoom left cascade>
@@ -14,7 +32,7 @@ const ManageAllOrderRow = ({ order, index, refetch, setDeletingItem }) => {
                 <td>{item}</td>
                 <td>{minimumOrder}/pcs</td>
                 <td>${totalPrice}</td>
-                <td>{paid && <button className="btn btn-xs btn-accent text-white">Pending</button>  }</td>
+                <td>{paid && <button onClick={() => handleChangeStatus(_id)} className="btn btn-xs btn-accent text-white">{status ? "Shipped" : "Pending"}</button>}</td>
                 <td>{paid ? <button className="btn btn-xs btn-primary text-white">Paid</button> : <button className="btn btn-xs btn-secondary text-white">Unpaid</button>}</td>
                 <td>{!paid && <label onClick={() => setDeletingItem(order)} htmlFor="all-order" className="btn btn-error btn-xs text-white modal-button">Delete</label>}</td>
             </Zoom>
--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -4,26 +4,24 @@ import Zoom from 'react-reveal/Zoom';
 
 const MyOrder = ({ order, setDeletingItem, index }) => {
     const { _id, userName, paid, email, item, minimumOrder, totalPrice, transactionId } = order;
-    
+
     return (
         <>
             <tr>
-                <Zoom left cascade>
-                    <th>{index + 1}</th>
-                    <td>{userName}</td>
-                    <td>{email}</td>
-                    <td>{item}</td>
-                    <td>${totalPrice}</td>
-                    <td>{minimumOrder}/pcs</td>
-                    <td>
-                        {(!paid) && <Link to={`/dashboard/payment/${_id}`}><button className='btn btn-xs btn-success text-white'>Pay</button></Link>}
-                        {(paid) && <div>
-                            <p><button className='btn btn-xs btn-primary text-white'>Paid</button></p>
-                            <p>Transaction Id: <span className='text-success'>{transactionId}</span> </p>
-                        </div>}
-                    </td>
-                    {!paid && <td><label onClick={() => setDeletingItem(order)} htmlFor="all-info" className="btn btn-error btn-xs text-white modal-button">Delete</label></td>}
-                </Zoom>
+                <th>{index + 1}</th>
+                <td>{userName}</td>
+                <td>{email}</td>
+                <td>{item}</td>
+                <td>${totalPrice}</td>
+                <td>{minimumOrder}/pcs</td>
+                <td>
+                    {(!paid) && <Link to={`/dashboard/payment/${_id}`}><button className='btn btn-xs btn-success text-white'>Pay</button></Link>}
+                    {(paid) && <div>
+                        <p><button className='btn btn-xs btn-primary text-white'>Paid</button></p>
+                        <p>Transaction Id: <span className='text-success'>{transactionId}</span> </p>
+                    </div>}
+                </td>
+                {!paid && <td><label onClick={() => setDeletingItem(order)} htmlFor="all-info" className="btn btn-error btn-xs text-white modal-button">Delete</label></td>}
             </tr>
         </>
     );
